036eb9b4189d282aef13b66bf893be2a316ba461
pduarteg <43941557+pduarteg@users.noreply.github.com>
2024-01-31 18:57:49 -0600

    v0.01
    - Proyecto inicial

---
+++ storage/framework/views/ff02e65e624e83aa0f3acea95039ac09.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>To Do List</title>
+</head>
+<body>
+	Bye world
+</body>
+</html><?php /**PATH C:\Users\Percy\Documents\Laravel_practice\ToDo_Laravel_project\resources\views/app.blade.php ENDPATH**/ ?>
